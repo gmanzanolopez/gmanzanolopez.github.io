aedc9b6c2ee11f932ec629bb1191138010e47b0b
Gabby Manzano <gabbymanzano98@gmail.com>
2020-08-06 19:29:23 -0500

    files have been modified

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -12,3 +12,26 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Returns random value from 0 to max -1
+//getRandomInt(9) -> returns value from 0 to 8
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+//var password = generatePassword(190,true,true,true,false,false);
+function generatePassword (length, lowercase, uppercase, numeric, special){
+  var characters = "";
+  if (lowercase) characters += "abcdefghijklmnopqrstuvwxyz";
+  if (uppercase) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (numeric) characters += "0123456789";
+  if (special) characters += "!@#$%^&*()";
+
+  var password = "";
+  for(var i = 0; i < length; i+=1)
+  {
+    var randCharacterIndex = getRandomInt(characters.length()); //get random character
+    password += characters[randCharacterIndex];
+  }
+  
+  return password;
+}
