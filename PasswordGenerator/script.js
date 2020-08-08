120d3e3653efa72f46ee1a930112f39141b2d0e3
Gabby Manzano <gabbymanzano98@gmail.com>
2020-08-08 16:48:15 -0500

    prompts were added

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -3,6 +3,46 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var wordlength = prompt ("Choose the length of password. From 8 to 50 characters")
+    if (wordlength > "8") {
+      wordlength = true;
+    }else (alert)
+
+  var wordLower = prompt ("Do you want Lowercase letters in your password? yes or no" )
+    if (wordLower === "yes") {
+    numbers = true;
+   }else {
+   numbers = false;
+   }
+
+  var wordUpper = prompt ("Do you want Uppercase letters in your password? yes or no")
+    if (wordUpper === "yes") {
+      wordUpper = true;
+    }else {
+      wordUpper = false;
+    }
+  
+  var wordNumber = prompt ("Do you want numbers? yes or no")
+    if (wordNumber === "yes") {
+    wordNumber = true;
+    }else {
+    wordNumber = false;
+    }
+  
+  var wordSpecial = prompt ("Do you want Special Characters? yes or no ")
+    if (wordNumber === "yes") {
+    wordNumber = true;
+    }else {
+    wordNumber = false;
+    }
+    
+    
+
+ 
+
+  
+  
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -24,7 +64,7 @@ function generatePassword (length, lowercase, uppercase, numeric, special){
   if (lowercase) characters += "abcdefghijklmnopqrstuvwxyz";
   if (uppercase) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   if (numeric) characters += "0123456789";
-  if (special) characters += "!@#$%^&*()";
+  if (special) characters += "!@#$%^&*+=:,./'?`~";
 
   var password = "";
   for(var i = 0; i < length; i+=1)
