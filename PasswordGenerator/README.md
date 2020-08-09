b8e66cbfd5f6ff06005cf3d4851d61aed7bf9828
Gabby Manzano <gabbymanzano98@gmail.com>
2020-08-08 23:25:04 -0500

    Added README.md to repo

---
+++ PasswordGenerator/README.md
@@ -0,0 +1,21 @@
+# Password Generator Homework
+  
+    I tried to  create an application that generates a password depending on the criteria the user selects. The page displays a series of questions with the criteria that you are able to choose from.
+
+# Generate password button
+  When you first click on the button that says generate password it will prompt 
+  you to a series of questions in order to be able to generate you a proper password.
+
+# Password Generator Questions 
+  * First it will ask you for the length of the password, which you get to choose
+  from 8 to 25 characters.
+  * Second it will ask if you want to add lower case letters to your password 
+  * Third it will ask if you want upper case letters.
+  * Fourth it will ask if you also want numbers.
+  * Last it will ask if you also want special characters in your passowrd.
+ This should be able to generate you an accurate password with the different 
+ criteria you choosed. 
+
+# Password Generating Process
+  I did all my javascript as cleaned as I could I was unable to make the special 
+  characters, upper and lower case letters to work. The length of password was accurate and numbers, other than that I tried my best to figure out how to make the other characters work.
--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -1,77 +1,69 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var wordlength = prompt ("Choose the length of password. From 8 to 50 characters")
-    if (wordlength > "8") {
-      wordlength = true;
-    }else (alert)
-
-  var wordLower = prompt ("Do you want Lowercase letters in your password? yes or no" )
-    if (wordLower === "yes") {
-    numbers = true;
-   }else {
-   numbers = false;
-   }
-
-  var wordUpper = prompt ("Do you want Uppercase letters in your password? yes or no")
-    if (wordUpper === "yes") {
-      wordUpper = true;
-    }else {
-      wordUpper = false;
-    }
-  
-  var wordNumber = prompt ("Do you want numbers? yes or no")
-    if (wordNumber === "yes") {
-    wordNumber = true;
-    }else {
-    wordNumber = false;
-    }
-  
-  var wordSpecial = prompt ("Do you want Special Characters? yes or no ")
-    if (wordNumber === "yes") {
-    wordNumber = true;
-    }else {
-    wordNumber = false;
-    }
-    
-    
-
- 
-
-  
-  
-  
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Returns random value from 0 to max -1
-//getRandomInt(9) -> returns value from 0 to 8
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-//var password = generatePassword(190,true,true,true,false,false);
-function generatePassword (length, lowercase, uppercase, numeric, special){
-  var characters = "";
-  if (lowercase) characters += "abcdefghijklmnopqrstuvwxyz";
-  if (uppercase) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  if (numeric) characters += "0123456789";
-  if (special) characters += "!@#$%^&*+=:,./'?`~";
 
-  var password = "";
-  for(var i = 0; i < length; i+=1)
-  {
-    var randCharacterIndex = getRandomInt(characters.length()); //get random character
-    password += characters[randCharacterIndex];
-  }
+// Write password to the #password input
+function writePassword() { 
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;}
+    //var password = generatePassword(50,true,true,true,false,false);
+  function generatePassword (length, wordLower, wordUpper, wordNumber, wordSpecial){
+    var values = []
+    if (wordLower) values += "abcdefghijklmnopqrstuvwxyz";
+    if (wordUpper) values += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (wordNumber) values += "0123456789";
+    if (wordSpecial) values += "!@#$%^&*+=:,./'?`~";
+
+    var wordLength = prompt ("Choose the length of password. From 8 to 50 characters")
+      if (wordLength > "8") {
+        wordLength = true;
+        console.log(wordLength);
+      }else {
+        
+      }
+
+    var wordLower = prompt ("Do you want Lowercase letters in your password? yes or no" )
+    if (wordLower === "yes") {
+      wordLower === true;
+      console.log(wordLower);
+    }else {
+      wordLower === false;
+    }
+
+    var wordUpper = prompt ("Do you want Uppercase letters in your password? yes or no")
+      if (wordUpper === "yes") {
+        wordUpper === true;
+        console.log(wordUpper);
+      }else {
+        wordUpper === false;
+      }
+    
+    var wordNumber = prompt ("Do you want numbers? yes or no")
+      if (wordNumber === "yes") {
+        wordNumber === true;
+        console.log(wordNumber);
+      }else {
+        wordNumber === false;
+      }
+    
+    var wordSpecial = prompt ("Do you want Special Characters? yes or no ")
+      if (wordSpecial === "yes") {
+        wordSpecial === true;
+        console.log(wordSpecial);
+      }else {
+        wordSpecial === false;
+      }
+      
   
-  return password;
-}
+    var password = "";
+    
+    for(var i = 0; i <= wordLength; i++) { 
+      var NewPass = [Math.floor(Math.random()* wordLength.length)];
+      password = NewPass + password;}
+      
+    
+    return password;}
