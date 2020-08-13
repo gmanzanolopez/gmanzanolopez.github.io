7377331b42509de5c43e13186351b35ee3cf770f
Gabby Manzano <gabbymanzano98@gmail.com>
2020-08-12 22:59:44 -0500

    added questions

---
+++ CodingQuiz/script.js
@@ -0,0 +1,33 @@
+var questions = [
+{
+    question: "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+    answerChoices: ["src", "title", "alt", "href"],
+    correctAnswer: "alt",
+},
+{
+    question: "Choose which HTML element is used to define the navigation links?",
+    answerChoices: ["<navigation>", "<nave>", "<nav>"],
+    correctAnswer: "<nav>",
+},
+{
+    question: "How can you add a background color for all <h1> elements?",
+    answerChoices: ["#h1 {background-color:#dddd;}", ".h1 {background-color:#dddd;}", "h1 {background-color=#dddd;}", "h1 {background-color:#dddd;}"],
+    correctAnswer: "h1 {background-color:#dddd;}",
+
+},
+{
+    question: "How do you start a FOR loop?",
+    answerChoices: ["for(i=0; i <=5; i++)", "for(i <=5: i+)", "for(i=0; i<5; i++)", "for(i++ <=5; i=0;)"],
+    correctAnswer: "for(i=0; i <=5; i++)",
+},
+{
+    question: "In Javascript, how do you declare a variable?",
+    answerChoices: ["variable textHere;", "var textHere;", "var= textHere;", "v: textHere;"],
+    correctAnswer: "var textHere;",
+},
+{
+    question: "Which class is used to create a big box for calling extra attention?",
+    answerchoices: [".container", ".jumbotron", ".wrapper",".popup"],
+    correctAnswer: ".jumbotron",
+},
+]
