5e41381a3e9d72e7416302a3387a3913e0d4359e
Gabby Manzano <gabbymanzano98@gmail.com>
2020-08-20 21:32:23 -0500

    updated README page

---
+++ DailyPlannerHelper/README.md
@@ -0,0 +1,5 @@
+# Planner Scheduler 
+  * Created a planner for the user to be able to input an event for each hour of the starting from 8am to 6pm. 
+  * After the user inputs there event they will be able to save the 
+  information and if the page is refreshed the previous events inputed will still appear.
+  
--- DailyPlannerHelper/script.js
+++ DailyPlannerHelper/script.js
@@ -1,30 +1,34 @@
 $(document).ready(function () {
   // create array for "business times"
   var availTimes = ['8am', '9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm']
-
+  var CurrentDate = moment().format('MMMM Do YYYY');
+     // adding new variables
   for (var i=0; i < availTimes.length; i++) {
       var newDiv = $("<div>");
       var timeDiv = $("<div>");
       var descriptionDiv = $("<textarea>");
       var buttonEle = $("<button>");
       var iconEle = $("<i>")
-
+      //setting attributes
       newDiv.attr('data-hours', availTimes[i]);
-      timeDiv.attr('class', 'row');
-      
+      newDiv.attr('class', 'row');
+
       descriptionDiv.attr('class', 'description col-8')
       timeDiv.attr('class', 'hour col-2')
       buttonEle.attr('class', 'saveBtn col-2');
       iconEle.attr('class', 'far fa-save');
-
+      
       $(".container").append(newDiv)
-
-      newDiv.append(timeDiv)
-      newDiv.append(descriptionDiv)
+      // appending divs
+      newDiv.append(timeDiv);
+      newDiv.append(descriptionDiv);
       newDiv.append(buttonEle)
 
       buttonEle.append(iconEle)
-      
       timeDiv.text(availTimes[i])
+
+
+
+
   }
 })
