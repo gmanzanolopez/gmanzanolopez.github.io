40694f37d34a6d4764ea7fcca8d0186ee2ef665e
Gabby Manzano <gabbymanzano98@gmail.com>
2020-08-22 15:30:59 -0500

    final result for app

--- DailyPlannerHelper/README.md
+++ DailyPlannerHelper/README.md
@@ -2,4 +2,13 @@
   * Created a planner for the user to be able to input an event for each hour of the starting from 8am to 6pm. 
   * After the user inputs there event they will be able to save the 
   information and if the page is refreshed the previous events inputed will still appear.
-  
+
+
+# Title 
+
+ The title will be displayed at the top of the page and will also have the current day. 
+
+
+# Planner App Final Results
+
+  [SCREENSHOT(107).png]
--- DailyPlannerHelper/script.js
+++ DailyPlannerHelper/script.js
@@ -1,34 +1,78 @@
 $(document).ready(function () {
-  // create array for "business times"
-  var availTimes = ['8am', '9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm']
-  var CurrentDate = moment().format('MMMM Do YYYY');
-     // adding new variables
-  for (var i=0; i < availTimes.length; i++) {
-      var newDiv = $("<div>");
-      var timeDiv = $("<div>");
-      var descriptionDiv = $("<textarea>");
-      var buttonEle = $("<button>");
-      var iconEle = $("<i>")
-      //setting attributes
-      newDiv.attr('data-hours', availTimes[i]);
-      newDiv.attr('class', 'row');
-
-      descriptionDiv.attr('class', 'description col-8')
-      timeDiv.attr('class', 'hour col-2')
-      buttonEle.attr('class', 'saveBtn col-2');
-      iconEle.attr('class', 'far fa-save');
-      
-      $(".container").append(newDiv)
-      // appending divs
-      newDiv.append(timeDiv);
-      newDiv.append(descriptionDiv);
-      newDiv.append(buttonEle)
-
-      buttonEle.append(iconEle)
-      timeDiv.text(availTimes[i])
-
-
-
-
-  }
-})
+    
+    var availTimes = [9, 10, 11, 12, 13, 14, 15, 16, 17,18]
+    var todayIs = moment().format('MMMM Do YYYY');
+    var localTime = moment().format('H');
+  
+    
+    $("#currentDay").append(todayIs)
+  
+  
+    for (var i = 0; i < availTimes.length; i++) {
+       
+        if (availTimes[i] / 12 > 1) {
+  
+            console.log(availTimes[i]);
+               dayHour = "PM";
+            console.log(availTimes[i] * 10);
+            hour = availTimes[i] - 12;
+        } else {
+            var hour = availTimes[i];
+            if (availTimes[i] == 12) {
+               dayHour = "PM"
+            } else {
+                dayHour = "AM"
+            };
+        };
+  
+  
+    // making new variables
+        var newDiv = $("<div>");
+        var timeDiv = $("<div>");
+        var descripDiv = $("<textarea>");
+        var buttonEle = $("<button>");
+        var iconEle = $("<i>");
+  
+  
+        
+        newDiv.attr("data-hour", availTimes[i]);
+        newDiv.attr("class", "row")
+        descripDiv.attr("class", "description col 8");
+        descripDiv.attr("id", "text" + [i]);
+        timeDiv.attr("class", "hour col-2");
+        buttonEle.attr("class", "saveBtn col-2");
+        buttonEle.attr("id", [i]);
+        iconEle.attr("class", "far fa-save");
+  
+  
+        
+        $(".container").append(newDiv)
+  
+        newDiv.append(timeDiv)
+        newDiv.append(descripDiv)
+        newDiv.append(buttonEle)
+        buttonEle.append(iconEle)
+  
+        $("#text" + [i]).append(
+            localStorage.getItem("yourInfo" + [i])
+        );
+  
+        timeDiv.text(hour + dayHour);
+  
+        // time changing colors for calendar
+        if (availTimes[i] == localTime) {
+            descripDiv.attr("class", "description col 8 present")
+        } else if (availTimes[i] < localTime) {
+            descripDiv.attr("class", "description col 2 past")
+        } else {
+            descripDiv.attr("class", "description col 2 future")
+        }
+    }
+    // save button function
+    $(".saveBtn").on("click", function () {
+        var planText = "yourInfo" + this.id;
+        var writeEle = "#text" + this.id;
+        localStorage.setItem(planText, $(writeEle).val());
+    })
+  
+  })
